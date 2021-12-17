8430e4ac35c32eedca967bfd70e2a3e9e798440a
Stefan Feldner <63292629+stefanfeldner@users.noreply.github.com>
2021-12-17 13:05:55 +0100

    Fetching data and updating state / UI

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import React from "react";
 import Intro from "./components/Intro";
 import Question from "./components/Question";
+import { nanoid } from "nanoid";
 
 function App() {
   const [showOverlay, setShowOverlay] = React.useState(true);
+  const [questions, setQuestions] = React.useState([]);
 
   const toggleOverlay = () => {
     setShowOverlay((prevState) => !prevState);
   };
 
+  React.useEffect(() => {
+    const fetchAPI = async () => {
+      try {
+        const response = await fetch(
+          "https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple"
+        );
+        const data = await response.json();
+        setQuestions(data.results);
+      } catch (err) {
+        throw new Error(err);
+      }
+    };
+    fetchAPI();
+  }, []);
+
+  const questionElements = questions.map((question) => {
+    return (
+      <Question
+        title={question.question}
+        correctAnswer={question.correct_answer}
+        incorrectAnswers={question.incorrect_answers}
+        key={nanoid()}
+      />
+    );
+  });
+
   return (
     <div className="container">
       {showOverlay && <Intro toggleOverlay={toggleOverlay} />}
-      <Question />
-      <Question />
+      {questionElements}
       <div className="result">
         <p>You scored 3/5 correct answers</p>
         <button className="checkAnswer">Check answers</button>
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,16 +1,65 @@
+import { decode } from "html-entities";
+import React from "react";
+import { nanoid } from "nanoid";
+
 function Question(props) {
+  const [answers, setAnswers] = React.useState([]);
+
+  React.useEffect(() => {
+    defineAnswers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const createAnswer = (answer, id, isHeld) => {
+    return {
+      answer,
+      id,
+      isHeld,
+    };
+  };
+
+  const defineAnswers = () => {
+    const allAnswers = [];
+    props.incorrectAnswers.forEach((answer) => {
+      allAnswers.push(createAnswer(answer, nanoid(), false));
+    });
+    allAnswers.push(createAnswer(props.correctAnswer, nanoid(), false));
+    setAnswers(allAnswers);
+  };
+
+  console.log(answers);
+
+  const handleClick = (id) => {
+    setAnswers((prevState) =>
+      prevState.map((answer) => {
+        if (answer.id === id) {
+          return {
+            ...answer,
+            isHeld: !answer.isHeld,
+          };
+        } else {
+          return answer;
+        }
+      })
+    );
+  };
+
+  const answerElements = answers.map((answer) => {
+    return (
+      <div
+        key={answer.id}
+        className={`question--answer ${answer.isHeld ? "active" : ""}`}
+        onClick={() => handleClick(answer.id)}
+      >
+        {decode(answer.answer)}
+      </div>
+    );
+  });
+
   return (
     <div className="question">
-      <div className="question--title">
-        Which best selling toy of 1983 caused hysteria, resulting in riots
-        breaking in stores?
-      </div>
-      <div className="question-answers">
-        <div className="question--answer">Adios</div>
-        <div className="question--answer">Hola</div>
-        <div className="question--answer">Au Revoir</div>
-        <div className="question--answer">Salir</div>
-      </div>
+      <div className="question--title">{decode(props.title)}</div>
+      <div className="question-answers">{answerElements}</div>
     </div>
   );
 }
